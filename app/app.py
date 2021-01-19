5a6bf421737adaf45abf1d1cd4db825e777adac8
TheRealJackiBoi <jslam@oulund.dk>
2021-01-19 01:03:38 +0100

    0.21

--- app/app.py
+++ app/app.py
@@ -21,7 +21,7 @@ def load_user(user_id):
     #the user_id is just the primary key of our user table, use it in the query for the user
     return User.query.get(int(user_id))
 
-
+#hello
 
 
 @app.route('/',methods=['GET','POST'])
